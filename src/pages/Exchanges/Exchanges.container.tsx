ca8f13e42c07bc82c89723a8f9489be2852b9de2
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-11-08 22:57:50 +0530

    define ExchangeType

--- src/pages/Exchanges/Exchanges.container.tsx
+++ src/pages/Exchanges/Exchanges.container.tsx
@@ -3,8 +3,19 @@ import React, { useState, useEffect } from "react";
 import { getExchanges } from "rest";
 import Exchanges from "./Exchanges";
 
+export interface ExchangeType {
+  id: string;
+  name: string;
+  country: string;
+  url: string;
+  image: string;
+  trust_score_rank: number;
+  trust_score: number;
+  year_established: number;
+}
+
 const ExchangesContainer = (): JSX.Element => {
-  const [exchanges, setExchanges] = useState([]);
+  const [exchanges, setExchanges] = useState<Array<ExchangeType>>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
