a2e476b04a0ea3bb21d2980a004450968bb40d1a
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-11-09 01:13:19 +0530

    implement data fetching and handle loading and error state

--- src/pages/ExchangeDetails/ExchangeDetails.container.tsx
+++ src/pages/ExchangeDetails/ExchangeDetails.container.tsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+import { getExchangeDetails } from "rest";
 import ExchangeDetails from "./ExchangeDetails";
 
+export interface ExchangeType {
+  name: string;
+  country: string;
+  url: string;
+  description?: string;
+  image: string;
+  trust_score_rank: number;
+  trust_score: number;
+  year_established: number;
+  facebook_url?: string;
+  slack_url?: string;
+  telegram_url?: string;
+  twitter_handle?: string;
+}
+
 const ExchangeDetailsContainer = (): JSX.Element => {
-  return <ExchangeDetails />;
+  const [exchange, setExchange] = useState<ExchangeType | object>({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const { exchangeId } = useParams();
+
+  useEffect(() => {
+    if (!exchangeId) {
+      return;
+    }
+
+    setIsLoading(true);
+    getExchangeDetails(exchangeId)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result, "RESULT");
+        setExchange(result);
+      })
+      .catch(() => {
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [exchangeId]);
+
+  return (
+    <ExchangeDetails
+      isLoading={isLoading}
+      isError={isError}
+      exchange={exchange}
+    />
+  );
 };
 
 export default ExchangeDetailsContainer;
--- src/pages/ExchangeDetails/ExchangeDetails.tsx
+++ src/pages/ExchangeDetails/ExchangeDetails.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
-const ExchangeDetails = (): JSX.Element => {
-  return <>Exchange Details</>;
+import { Loader, Error } from "components";
+import { ExchangeType } from "./ExchangeDetails.container";
+import styles from "./ExchangeDetails.module.css";
+
+interface PropTypes {
+  isLoading: boolean;
+  isError: boolean;
+  exchange: ExchangeType | object;
+}
+
+const ExchangeDetails = ({
+  isLoading,
+  isError,
+  exchange,
+}: PropTypes): JSX.Element => {
+  if (isLoading) {
+    return (
+      <div className={styles.loaderWrapperContainer}>
+        <Loader />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className={styles.errorWrapperContainer}>
+        <Error />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {/* <header>Hello</header> */}
+      <main>main h</main>
+    </div>
+  );
 };
 
 export default ExchangeDetails;
