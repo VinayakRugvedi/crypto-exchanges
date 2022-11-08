8653ddb140c3af3e18ecb8f8bc613c90922e6e1a
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-11-08 13:14:44 +0530

    create dedicated directory for root App files

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { Exchanges } from "pages";
+
+function App() {
+  return (
+    <div className="app-container"> 
+      <Exchanges />
+    </div>
+  );
+}
+
+export default App;
