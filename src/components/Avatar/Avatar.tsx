358e2984b44ae2fdfe59e590af56ab5713fcd211
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-11-09 11:50:12 +0530

    make use of types and extend styles

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
+import { SizeTypes } from "types";
 import styles from "./Avatar.module.css";
 
 interface PropTypes {
   imageUrl?: string;
   alt?: string;
+  size?: string;
 }
 
-const Avatar = ({ imageUrl, alt = "avatar" }: PropTypes): JSX.Element => {
-  return <img className={styles.avatar} src={imageUrl} alt={alt} />;
+const Avatar = ({
+  imageUrl,
+  alt = "avatar",
+  size = SizeTypes.Small,
+}: PropTypes): JSX.Element => {
+  return (
+    <img
+      className={`${styles.avatar} ${styles[size]}`}
+      src={imageUrl}
+      alt={alt}
+    />
+  );
 };
 
 export default Avatar;
