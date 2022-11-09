63141c51fd938b4fafa7a2c326f4d1a2bc17ed65
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-11-09 11:54:25 +0530

    use Header component, add relevant sections and extend styling

--- src/pages/ExchangeDetails/ExchangeDetails.tsx
+++ src/pages/ExchangeDetails/ExchangeDetails.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import { Loader, Error } from "components";
 import { ExchangeType } from "./ExchangeDetails.container";
+import Header from "./components/Header";
 import styles from "./ExchangeDetails.module.css";
 
 interface PropTypes {
   isLoading: boolean;
   isError: boolean;
-  exchange: ExchangeType | object;
+  exchange: ExchangeType;
 }
 
 const ExchangeDetails = ({
@@ -33,8 +34,23 @@ const ExchangeDetails = ({
 
   return (
     <div>
-      {/* <header>Hello</header> */}
-      <main>main h</main>
+      <Header
+        name={exchange.name}
+        url={exchange.url}
+        imageUrl={exchange.image}
+        country={exchange.country}
+        yearEstablished={exchange.year_established}
+      />
+      <main>
+        <section>trust details</section>
+        <section>Social Media</section>
+        <section className={styles.descriptionSectionContainer}>
+          <h3 className={styles.descriptionHeaderText}>Description</h3>
+          <p className={styles.descriptionContent}>
+            {exchange.description ? exchange.description : "N/A"}
+          </p>
+        </section>
+      </main>
     </div>
   );
 };
