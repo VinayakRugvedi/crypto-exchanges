70b08301ab7f52759d117663ff77d10399c36720
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-11-09 11:47:36 +0530

    create BackLink component to handle internal back navigation

---
+++ src/components/BackLink/BackLink.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import styles from "./BackLink.module.css";
+
+interface PropTypes {
+  text?: string;
+  link?: string;
+}
+
+const BackLink = ({
+  text = "Back to main page",
+  link = "/",
+}: PropTypes): JSX.Element => {
+  return (
+    <Link className={styles.backLink} to={link}>
+      <span className={styles.backIcon}>
+        <FaLongArrowAltLeft />
+      </span>
+      &nbsp;&nbsp;
+      <span>{text}</span>
+    </Link>
+  );
+};
+
+export default BackLink;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Header from "./Header";
 import Loader from "./Loader";
 import Error from "./Error";
 import Avatar from "./Avatar";
+import BackLink from "./BackLink";
 
-export { Header, Loader, Error, Avatar };
+export { Header, Loader, Error, Avatar, BackLink };
